8510424bbfcb419289008ba236dad06a3abab80a
Cristina Mastellaro <cristinamastellaro8@gmail.com>
2025-10-27 16:31:00 +0100

    find all and find single address

--- src/main/java/team5/BW_CMR/controllers/IndirizzoController.java
+++ src/main/java/team5/BW_CMR/controllers/IndirizzoController.java
@@ -10,6 +10,9 @@ import team5.BW_CMR.exceptions.ValidationException;
 import team5.BW_CMR.payloads.IndirizzoDTO;
 import team5.BW_CMR.services.IndirizzoService;
 
+import java.util.List;
+import java.util.UUID;
+
 @RestController
 @RequestMapping("/address")
 public class IndirizzoController {
@@ -22,6 +25,16 @@ public class IndirizzoController {
         if (validation.hasErrors())
             throw new ValidationException(validation.getFieldErrors().stream().map(fL -> fL.getDefaultMessage()).toList());
         return iServ.saveIndirizzo(newAddress);
+    }
+
+    @GetMapping("/{indirizzoId}")
+    public Indirizzo findIndirizzoById(@PathVariable UUID indirizzoId) {
+        return iServ.findIndirizzoById(indirizzoId);
+    }
+
+    @GetMapping
+    public List<Indirizzo> findAllAddresses() {
+        return iServ.findAllAddresses();
     }
 
 }
--- src/main/java/team5/BW_CMR/services/IndirizzoService.java
+++ src/main/java/team5/BW_CMR/services/IndirizzoService.java
@@ -5,8 +5,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import team5.BW_CMR.entities.Comune;
 import team5.BW_CMR.entities.Indirizzo;
+import team5.BW_CMR.exceptions.NotFoundException;
 import team5.BW_CMR.payloads.IndirizzoDTO;
 import team5.BW_CMR.repositories.IndirizzoRepository;
+
+import java.util.List;
+import java.util.UUID;
 
 @Service
 @Slf4j
@@ -17,7 +21,7 @@ public class IndirizzoService {
     private ComuneService cServ;
 
     public Indirizzo saveIndirizzo(IndirizzoDTO newIndirizzo) {
-        // Non ci sono particolari controlli da fare
+        // Non ci sono particolari controlli da fare, se non controllare che il comune esiste
         Comune comune = cServ.findComuneByDenominazione(newIndirizzo.comune());
 
         Indirizzo indirizzo = new Indirizzo(newIndirizzo.via(), newIndirizzo.civico(), newIndirizzo.localita(), newIndirizzo.cap(), comune);
@@ -27,5 +31,13 @@ public class IndirizzoService {
         log.info("L'indirizzo " + indirizzo.getVia() + " è stato salvato correttamente!");
 
         return indirizzo;
+    }
+
+    public Indirizzo findIndirizzoById(UUID id) {
+        return iRepo.findById(id).orElseThrow(() -> new NotFoundException("L'indirizzo con id " + id + " non è stato trovato"));
+    }
+
+    public List<Indirizzo> findAllAddresses() {
+        return iRepo.findAll();
     }
 }
