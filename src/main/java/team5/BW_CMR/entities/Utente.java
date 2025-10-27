c2a90cde19d564df8b31a1585adbae76d7d207ab
Leonardo2397 <leonardoferrante753@gmail.com>
2025-10-27 16:17:00 +0100

    edit1

---
+++ src/main/java/team5/BW_CMR/entities/Utente.java
@@ -0,0 +1,54 @@
+package team5.BW_CMR.entities;
+
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Entity
+@Table(name = "utenti")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Utente {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Setter(AccessLevel.NONE)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String username;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @JsonIgnore
+    @Column(nullable = false)
+    private String password;
+
+    @Column
+    private String firstname;
+
+    @Column
+    private  String lastname;
+
+    @Column
+    private String avatarUrl;
+
+    @ManyToMany
+    @JoinTable(
+            name = "ruoli_utente",
+            joinColumns = @JoinColumn(name = "utente_id"),
+            inverseJoinColumns = @JoinColumn(name = "Ruolo_id")
+    )
+    private Set<Ruolo> ruoli = new HashSet<>();
+
+    public void addRuolo(Ruolo ruolo) {
+        this.ruoli.add(ruolo);
+    }
+
+}
---
+++ src/main/java/team5/BW_CMR/repositories/UtenteRepository.java
@@ -0,0 +1,18 @@
+package team5.BW_CMR.repositories;
+
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import team5.BW_CMR.entities.Utente;
+
+import java.util.Optional;
+
+@Repository
+public interface UtenteRepository extends JpaRepository<Utente, Long> {
+
+    Optional<Utente> findByEmail(String email);
+    Optional<Utente> findByUsername(String username);
+
+    Boolean existsByEmail(String email);
+    Boolean existsByUsername(String username);
+}
