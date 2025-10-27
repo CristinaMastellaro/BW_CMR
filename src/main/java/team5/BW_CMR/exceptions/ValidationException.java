1a7e8b9d28dfeadd9afb317bd790fdb324e85d14
Cristina Mastellaro <cristinamastellaro8@gmail.com>
2025-10-27 15:39:18 +0100

    fix merge

--- src/main/java/team5/BW_CMR/exceptions/ValidationException.java
+++ src/main/java/team5/BW_CMR/exceptions/ValidationException.java
@@ -3,11 +3,12 @@ package team5.BW_CMR.exceptions;
 import lombok.Getter;
 
 import java.util.List;
+
 @Getter
 public class ValidationException extends RuntimeException {
     private List<String> errors;
-    public ValidationException(List<String> errors) {
 
+    public ValidationException(List<String> errors) {
         super("Errori nella validazoione del payload");
         this.errors = errors;
     }
