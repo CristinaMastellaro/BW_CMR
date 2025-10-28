a785c584858ae577a97858ea80b56ef3330ae8a4
Leonardo2397 <leonardoferrante753@gmail.com>
2025-10-28 12:58:00 +0100

    edit6

---
+++ src/main/java/team5/BW_CMR/services/CustomUserDetailsService.java
@@ -0,0 +1,22 @@
+package team5.BW_CMR.services;
+
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+import team5.BW_CMR.repositories.UtenteRepository;
+
+@Service
+public class CustomUserDetailsService implements UserDetailsService {
+
+    private final UtenteRepository utenteRepository;
+
+    public CustomUserDetailsService(UtenteRepository utenteRepository) {
+        this.utenteRepository = utenteRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return utenteRepository.findByUsername(username).orElseThrow(() -> new UsernameNotFoundException("utente non trovato:" + username));
+    }
+}
