0e733eaa1383353bdffa89e3a7e965b9a9a91000
Anna De Lise <anna_delise@libero.it>
2025-10-30 22:03:34 +0100

    add register css

--- BW-CRM/src/components/FormRegister.jsx
+++ BW-CRM/src/components/FormRegister.jsx
@@ -2,6 +2,7 @@ import Button from "react-bootstrap/Button";
 import { Container, Row, Col, Form, Alert } from "react-bootstrap";
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
+import pp from '../assets/pp.jpg'
 
 const FormRegister = () => {
   const [username, setUsername] = useState("");
@@ -53,12 +54,15 @@ const FormRegister = () => {
 
   return (
     <>
-      <Container>
-        <Row className="justify-content-center mt-5">
-          <Col xs={12} md={8}>
+      <Container fluid className="p-5 font-nunito">
+        <Row className="text-center justify-content-center"><Col xs={8}>
+        <img className="w-100" src={pp}/></Col></Row>
+        <Row className="justify-content-center border-row p-2 pb-4">
+          <Col xs={10} className="text-center"><h1 className=" fw-bolder">Sign up!</h1></Col>
+          <Col xs={10} md={6} className="bg-white">
             <Form onSubmit={saveForm}>
               <Form.Group className="mb-3" controlId="username">
-                <Form.Label>Username</Form.Label>
+                <Form.Label className="fw-bolder text-pink">Username</Form.Label>
                 <Form.Control
                   type="text"
                   placeholder="Enter username"
@@ -69,7 +73,7 @@ const FormRegister = () => {
               </Form.Group>
 
               <Form.Group className="mb-3" controlId="email">
-                <Form.Label>Email address</Form.Label>
+                <Form.Label className="fw-bolder">Email address</Form.Label>
                 <Form.Control
                   type="email"
                   placeholder="Enter email"
@@ -80,7 +84,7 @@ const FormRegister = () => {
               </Form.Group>
 
               <Form.Group className="mb-3" controlId="formBasicPassword">
-                <Form.Label>Password</Form.Label>
+                <Form.Label className="fw-bolder text-pink">Password</Form.Label>
                 <Form.Control
                   type="password"
                   placeholder="Password"
@@ -91,7 +95,7 @@ const FormRegister = () => {
               </Form.Group>
 
               <Form.Group className="mb-3" controlId="firstName">
-                <Form.Label>First name</Form.Label>
+                <Form.Label className="fw-bolder">First name</Form.Label>
                 <Form.Control
                   type="text"
                   placeholder="Enter firstname"
@@ -102,7 +106,7 @@ const FormRegister = () => {
               </Form.Group>
 
               <Form.Group className="mb-3" controlId="lastName">
-                <Form.Label>Last name</Form.Label>
+                <Form.Label className="fw-bolder text-pink">Last name</Form.Label>
                 <Form.Control
                   type="text"
                   placeholder="Enter last name"
@@ -112,7 +116,7 @@ const FormRegister = () => {
                 />
               </Form.Group>
 
-              <Button variant="primary" type="submit">
+              <Button className="btn-pink d-block mx-auto" type="submit">
                 Submit
               </Button>
             </Form>
