9609f6c57e03f6f56dcf440697ceb6b4a7e2457d
Cristina Mastellaro <cristinamastellaro8@gmail.com>
2025-10-31 15:03:59 +0100

    wip card

--- BW-CRM/src/components/Clienti.jsx
+++ BW-CRM/src/components/Clienti.jsx
@@ -146,7 +146,8 @@ const Clienti = () => {
               <strong className="d-block mb-2 fs-5">Order by</strong>
               <Form
                 onSubmit={ordinaClienti}
-                className="d-flex flex-column flex-md-row justify-content-between align-items-center">
+                className="d-flex flex-column flex-md-row justify-content-between align-items-center"
+              >
                 {["radio"].map((type) => (
                   <div key={`inline-${type}`}>
                     <Form.Check
@@ -212,123 +213,160 @@ const Clienti = () => {
               </Form>
             </Col>
             <hr className="mt-4" />
-            <Col xs={10} className="mb-2"> 
-             <Form onSubmit={cercaClienti}>
-  <Row>
-    <Col md={6}>
-      <Form.Group className="mb-2" controlId="nomeContatto">
-        <Form.Label className="fw-bolder text-blue">
-          Contact name
-        </Form.Label>
-        <Form.Control
-          type="text"
-          placeholder="Enter contact name"
-          value={nomeContatto}
-          onChange={(e) => setNomeContatto(e.target.value)}
-          size="sm"
-        />
-      </Form.Group>
+            <Col xs={10} className="mb-2">
+              <Form onSubmit={cercaClienti}>
+                <Row>
+                  <Col md={6}>
+                    <Form.Group className="mb-2" controlId="nomeContatto">
+                      <Form.Label className="fw-bolder text-blue">
+                        Contact name
+                      </Form.Label>
+                      <Form.Control
+                        type="text"
+                        placeholder="Enter contact name"
+                        value={nomeContatto}
+                        onChange={(e) => setNomeContatto(e.target.value)}
+                        size="sm"
+                      />
+                    </Form.Group>
 
-      <Form.Group className="mb-2" controlId="dataInserimento">
-        <Form.Label className="fw-bolder">
-          Date of entry
-        </Form.Label>
-        <Form.Control
-          type="text"
-          placeholder="Enter date of entry"
-          value={dataInserimento}
-          onChange={(e) => setDataInserimento(e.target.value)}
-          size="sm"
-        />
-      </Form.Group>
+                    <Form.Group className="mb-2" controlId="dataInserimento">
+                      <Form.Label className="fw-bolder">
+                        Date of entry
+                      </Form.Label>
+                      <Form.Control
+                        type="text"
+                        placeholder="Enter date of entry"
+                        value={dataInserimento}
+                        onChange={(e) => setDataInserimento(e.target.value)}
+                        size="sm"
+                      />
+                    </Form.Group>
 
-      <Form.Group className="mb-2" controlId="provincia">
-        <Form.Label className="fw-bolder text-blue ">
-          District of legal address
-        </Form.Label>
-        <Form.Control
-          type="text"
-          placeholder="Enter district of legal address"
-          value={provincia}
-          onChange={(e) => setProvincia(e.target.value)}
-          size="sm"
-        />
-      </Form.Group>
-    </Col>
+                    <Form.Group className="mb-2" controlId="provincia">
+                      <Form.Label className="fw-bolder text-blue ">
+                        District of legal address
+                      </Form.Label>
+                      <Form.Control
+                        type="text"
+                        placeholder="Enter district of legal address"
+                        value={provincia}
+                        onChange={(e) => setProvincia(e.target.value)}
+                        size="sm"
+                      />
+                    </Form.Group>
+                  </Col>
 
-    <Col md={6}>
-      <Form.Group className="mb-2" controlId="fatturato">
-        <Form.Label className="fw-bolder ">
-          Annual turnover
-        </Form.Label>
-        <Form.Control
-          type="text"
-          placeholder="Enter annual turnover"
-          value={fatturato}
-          onChange={(e) => setFatturato(e.target.value)}
-          size="sm"
-        />
-      </Form.Group>
+                  <Col md={6}>
+                    <Form.Group className="mb-2" controlId="fatturato">
+                      <Form.Label className="fw-bolder ">
+                        Annual turnover
+                      </Form.Label>
+                      <Form.Control
+                        type="text"
+                        placeholder="Enter annual turnover"
+                        value={fatturato}
+                        onChange={(e) => setFatturato(e.target.value)}
+                        size="sm"
+                      />
+                    </Form.Group>
 
-      <Form.Group className="mb-2" controlId="dataUltimoContatto">
-        <Form.Label className="fw-bolder text-blue">
-          Date of last contact
-        </Form.Label>
-        <Form.Control
-          type="text"
-          placeholder="Enter date of last contact"
-          value={dataUltimoContatto}
-          onChange={(e) => setDataUltimoContatto(e.target.value)}
-          size="sm"
-        />
-      </Form.Group>
-      <Button
-    type="submit"
-    className="btn-blue d-block ms-auto mt-5"
-    size="sm">
-    Filter
-  </Button>
-    </Col>
-  </Row> 
-</Form>
+                    <Form.Group className="mb-2" controlId="dataUltimoContatto">
+                      <Form.Label className="fw-bolder text-blue">
+                        Date of last contact
+                      </Form.Label>
+                      <Form.Control
+                        type="text"
+                        placeholder="Enter date of last contact"
+                        value={dataUltimoContatto}
+                        onChange={(e) => setDataUltimoContatto(e.target.value)}
+                        size="sm"
+                      />
+                    </Form.Group>
+                    <Button
+                      type="submit"
+                      className="btn-blue d-block ms-auto mt-5"
+                      size="sm"
+                    >
+                      Filter
+                    </Button>
+                  </Col>
+                </Row>
+              </Form>
             </Col>
           </Row>
-         <Row className="justify-content-center mt-5 g-3 px-5 py-2">
-          <h1 className="text-center fw-bold">Clients</h1>
-          {clienti.map((cliente) => (
-            <Col key={cliente.id} xs={11} md={6} lg={4}>
-              <Card className="py-2 " style={{"border-radius":"10px"}}>
-              
-                <Card.Body className="d-flex flex-column align-items-center">
-                  <div className="d-flex justify-content-start mb-3">
-              
-                  <Card.Img className="me-3 rounded-circle" style={{"width":"5rem", "height":"5rem"}} src={cliente.logoAziendale} />
-                  <div className="d-flex flex-column  justify-content-center">
-                  <Card.Title className="fs-3 fw-bold mb-0 mt-2">{cliente.nomeContatto}</Card.Title>
-                  <Card.Text className="mb-2 fs-5">{cliente.ragioneSociale}</Card.Text>
-                  </div>
-                  </div>
-                  <div>
-                  <Card.Text className="mb-0">
-                    Email: {cliente.email}
-                  </Card.Text>
-                   <Card.Text className="mb-0">Annual Turnover: {cliente.fatturatoAnnuale}</Card.Text>
-                   <Card.Text className="mb-0">VAT Number: {cliente.partitaIva}</Card.Text>
-                   <Card.Text className="mb-0">Entry Date: {cliente.dataInserimento}</Card.Text>
-                   <Card.Text className="mb-0">Last Contact date: {cliente.dataUltimoContatto}</Card.Text>
-                   <Card.Text className="mb-0">PEC: {cliente.pec}</Card.Text>
-                   <Card.Text className="mb-0">Phone Number: {cliente.telefono}</Card.Text>
-                   <p className="mt-1 fs-5 mb-0 fw-bolder" >Contact</p>
-                   <Card.Text className="mb-0">Email: {cliente.emailContatto}</Card.Text>
-                   <Card.Text className="mb-0">Name: {cliente.nomeContatto}</Card.Text>
-                   <Card.Text className="mb-0">Surname: {cliente.cognomeContatto}</Card.Text>
-                   <Card.Text className="mb-0">Phone Number: {cliente.telefonoContatto}</Card.Text>
-                   </div>
-                </Card.Body>
-              </Card>
-            </Col>
-          ))}
-        </Row>
+          <Row className="justify-content-center mt-3 g-3 px-5 py-2">
+            <h1 className="text-center fw-bold">Clients</h1>
+            {clienti.map((cliente) => (
+              <Col key={cliente.id} xs={11} md={6} lg={4}>
+                <Card
+                  className="py-2"
+                  style={{
+                    "border-radius": "40px",
+                    border: "2px solid #1424ffaf",
+                  }}
+                >
+                  <Card.Body className="d-flex flex-column align-items-center">
+                    <div className="d-flex justify-content-start mb-4 mt-2">
+                      <Card.Img
+                        className="me-3 rounded-circle"
+                        style={{
+                          width: "5rem",
+                          height: "5rem",
+                          border: "2px solid #1424ffaf",
+                        }}
+                        src={cliente.logoAziendale}
+                      />
+                      <div className="d-flex flex-column  justify-content-center">
+                        <Card.Title className="fs-3 fw-bold mb-0 mt-2 text-blue">
+                          {cliente.nomeContatto}
+                        </Card.Title>
+                        <Card.Text className="mb-2 fs-5">
+                          {cliente.ragioneSociale}
+                        </Card.Text>
+                      </div>
+                    </div>
+                    <div className="border-top pt-4 ">
+                      <Card.Text className="mb-0">
+                        Email: {cliente.email}
+                      </Card.Text>
+                      <Card.Text className="mb-0">
+                        Annual Turnover: {cliente.fatturatoAnnuale}
+                      </Card.Text>
+                      <Card.Text className="mb-0">
+                        VAT Number: {cliente.partitaIva}
+                      </Card.Text>
+                      <Card.Text className="mb-0">
+                        Entry Date: {cliente.dataInserimento}
+                      </Card.Text>
+                      <Card.Text className="mb-0">
+                        Last Contact date: {cliente.dataUltimoContatto}
+                      </Card.Text>
+                      <Card.Text className="mb-0">PEC: {cliente.pec}</Card.Text>
+                      <Card.Text className="mb-0">
+                        Phone Number: {cliente.telefono}
+                      </Card.Text>
+                      <p className="mt-3 fs-5 mb-0 fw-bolder text-blue">
+                        Contact
+                      </p>
+                      <Card.Text className="mb-0">
+                        Email: {cliente.emailContatto}
+                      </Card.Text>
+                      <Card.Text className="mb-0">
+                        Name: {cliente.nomeContatto}
+                      </Card.Text>
+                      <Card.Text className="mb-0">
+                        Surname: {cliente.cognomeContatto}
+                      </Card.Text>
+                      <Card.Text className="mb-0">
+                        Phone Number: {cliente.telefonoContatto}
+                      </Card.Text>
+                    </div>
+                  </Card.Body>
+                </Card>
+              </Col>
+            ))}
+          </Row>
         </div>
       </Container>
     </>
